88d2a62e43e491a0bfd5d401347ab03e358a3e62
unknown <juanjosesierram@gmail.com>
2024-02-02 12:18:29 -0500

    Loops

---
+++ src/loops/Loops.kt
@@ -0,0 +1,38 @@
+package loops
+
+fun main() {
+    /*
+    for (i in 1..10) {
+        println(i)
+    }
+
+    val list = listOf("Java", "Kotlin", "Python")
+    for (element in list) {
+        println(element)
+    }
+
+    for ((index, value) in list.withIndex()) {
+        println("Element at $index is $value")
+    }
+
+
+    var x = 9
+    //Can use break and continue
+    while (x >= 0) {
+        println(x)
+        x -= 1
+    }
+
+    //Named loops
+    outer@ for (i in 1..10) {
+        for (j in 1..10) {
+            if (i - j == 5) {
+                break@outer
+            }
+            println("$i - $j")
+        }
+    }
+     */
+
+    //Challenge
+}
