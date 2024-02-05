69a02dfc6f323c109beffd91943fb0e03ca80a02
unknown <juanjosesierram@gmail.com>
2024-02-05 15:02:20 -0500

    Generics

--- src/oo/Stack.kt
+++ src/oo/Stack.kt
@@ -1,2 +1,32 @@
 package oo
 
+//Generics
+class Stack<E>(vararg val items: E) {
+
+    val elements = items.toMutableList()
+
+    fun push(element: E) {
+        elements.add(element)
+    }
+
+    fun pop(): E? {
+        if (!isEmpty()) {
+            return elements.removeAt(elements.size - 1)
+        }
+        return null
+    }
+
+    fun isEmpty(): Boolean {
+        return elements.isEmpty()
+    }
+}
+
+fun <T> stackOf(vararg elements: T): Stack<T> {
+    //Spread operator
+    return Stack(*elements)
+}
+
+fun main() {
+    val stack = Stack(3, 2, 5, 8)
+    println(stack.pop())
+}
