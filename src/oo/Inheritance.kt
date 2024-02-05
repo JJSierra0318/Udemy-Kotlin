28faacefc4ccc6e66daafecf63e311d51d577da4
unknown <juanjosesierram@gmail.com>
2024-02-05 11:20:59 -0500

    Abstract

--- src/oo/Inheritance.kt
+++ src/oo/Inheritance.kt
@@ -1,6 +1,9 @@
 package oo
 
-open class Human(open val name: String, open var age: Int) {
+//Abstract inherit but cant use father class
+abstract class Human(open val name: String, open var age: Int) {
+
+    abstract fun speak()
 
     fun greet(name: String) {
         println("Hello $name!")
@@ -10,11 +13,15 @@ open class Human(open val name: String, open var age: Int) {
 }
 
 class Student(override val name: String, override var age: Int, val studentID: String): Human(name, age) {
-
+    override fun speak() {
+        println("Hi there, I'm a student")
+    }
 }
 
 class Employee(override val name: String, override var age: Int): Human(name, age) {
-
+    override fun speak() {
+        println("Hi I'm an employee")
+    }
 }
 
 fun main() {
